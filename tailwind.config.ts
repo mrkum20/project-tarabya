2123138688756e5795c030503253ef9710e5c583
mrkum20 <kummustafayasir@gmail.com>
2025-12-16 09:51:32 +0300

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'pulse': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+      },
+      transitionDelay: {
+        '2000': '2000ms',
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
